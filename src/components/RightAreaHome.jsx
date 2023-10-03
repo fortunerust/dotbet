1141fc1c4c987ca12e1a0a127816cf224839f232
fortunerust <kayezpavi954@gmx.us>
2023-10-03 18:00:55 +0900

    menu bar action

--- src/components/RightAreaHome.jsx
+++ src/components/RightAreaHome.jsx
@@ -17,6 +17,9 @@ import { Roulette } from './Roulette'
 import { Slots } from './Slots'
 import { Sports } from './Sports'
 import { NewArrival } from './NewArrival'
+import menuExpander from '../assets/img/menu.svg'
+import Support from '../assets/img/aside/Support.svg'
+import blog from '../assets/img/aside/blog.svg'
 
 import sports from '../assets/img/aside/tabs/Sports.svg'
 import roulette from '../assets/img/aside/tabs/roulette.svg'
@@ -103,7 +106,10 @@ export const RightAreaHome = () => {
   const [rouletteState, setRouletteState] = useState(false)
   const [fishingState, setFishingState] = useState(false)
   const [showState, setShowState] = useState(false)
+  const [blogState, setBlogState] = useState(false)
+  const [supportState, setSupportState] = useState(false)
   const [openedFaqId, setOpenedFaqId] = useState(7)
+  const [supportActiveState, setSupportActiveState] = useState(2)
 
   const dropDownText = e => {
     setOpenedFaqId(e)
@@ -176,6 +182,22 @@ export const RightAreaHome = () => {
     }
   }
 
+  const supportAction = async e => {
+    setSupportActiveState(e)
+    switch (e) {
+      case 0:
+        await setBlogState(true)
+        await setSupportState(false)
+        break
+      case 1:
+        await setBlogState(false)
+        await setSupportState(true)
+        break
+      default:
+        break
+    }
+  }
+
   const gameListState = [
     {
       Img: home,
@@ -214,6 +236,17 @@ export const RightAreaHome = () => {
     }
   ]
 
+  const supportList = [
+    {
+      Img: blog,
+      Text: 'Blog'
+    },
+    {
+      Img: Support,
+      Text: 'Support'
+    }
+  ]
+
   useEffect(() => {
     if (rollingNumber) {
       let i = rollingNumber
@@ -229,173 +262,213 @@ export const RightAreaHome = () => {
     window.addEventListener('resize', ResizeWork)
   }, [])
   return (
-    <div className='flex-1'>
-      <Header />
-      <div className='body-area flex items-start px-6 pt-6'>
-        <div className='body-left-area  mr-6' ref={ref}>
-          <div className='welcome-area py-8 p-6 rounded-xl relative'>
-            <h1 className='mb-4'>Lucky Spin Tournament</h1>
-            <p className='my-3'>
-              Win in selected Smartoft games. Save points and share{' '}
-            </p>
-            <p className='text-start text-2xl py-4 w-full  rounded-xl rolling my-3 font-extrabold'>
-              ${' '}
-              {rollingNumber
-                .toString()
-                .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}{' '}
-            </p>
-            <p>1.09-26.09</p>
-            <div className='buttons-wrapper mt-6'>
-              <button className='w-36 h-11 mr-3'>Get Started</button>
-            </div>
-
-            <img src={rewardBanner} className='rewardBanner' alt='' />
-          </div>
-
-          <div className='tags-wrapper mt-10 mb-12  grid grid-cols-7 gap-3'>
-            {gameListState.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  onClick={e => dropDownText(index)}
-                  // href={`${item.id}`}
-                  className={`${
-                    index === openedFaqId ? 'active' : ''
-                  } tag flex justify-center  items-center cursor-pointer`}
-                >
-                  <img src={item.Img} alt='game show' />
-                  <p>{item.Text}</p>
-                </div>
-              )
-            })}
-          </div>
-
-          {homeState && (
-            <>
-              {/* RecentWin*/}
-              <Hits />
-
-              {/* NewArrival */}
-              <NewArrival />
-              {/* Sports */}
-              <Sports />
-
-              {/* LiveCasino */}
-              <LiveCasino />
-
-              {/* Slots */}
-              <Slots />
-
-              {/* Roulette */}
-              <Roulette />
-
-              {/* Fishing */}
-              <Fishing />
-
-              {/* GameShow */}
-              <GameShow />
-
-              {/* Top Rates Game */}
-              <TopRatesGame />
-
-              {/* LatestBets */}
-              <LatestBets />
-
-              {/* Recommended Game */}
-              <RecommendedGames />
-            </>
-          )}
-
-          {sportState && (
-            <>
-              {/* Sports */}
-              <Sports />
-            </>
-          )}
-
-          {liveState && (
-            <>
-              {/* LiveCasino */}
-              <LiveCasino />
-            </>
-          )}
-          {slotState && (
-            <>
-              {/* Slots */}
-              <Slots />
-            </>
-          )}
-          {rouletteState && (
-            <>
-              {/* Roulette */}
-              <Roulette />
-            </>
-          )}
-          {fishingState && (
-            <>
-              {/* Fishing */}
-              <Fishing />
-            </>
-          )}
-
-          {showState && (
-            <>
-              {/* GameShow */}
-              <GameShow />
-            </>
-          )}
+    <>
+      <aside className='px-5 py-4 flex flex-col'>
+        <div className='flex items-center justify-center mb-11 mt-2'>
+          <img src={menuExpander} alt='menuExpander' />
         </div>
-        <div className='body-right-area rounded-2xl p-4'>
-          <div className='top-area pb-4 flex items-center relative'>
-            <img src={chat} alt='' />
-            <h1 className='flex-1 ml-1'>General Chat</h1>
-            <p className='mr-1 absolute'>489</p>
-            <div className='flex items-center'>
-              <img
-                src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80'
-                alt='first user online'
-                className='w-5 h-5 rounded-full'
-              />
-              <img
-                src='https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80'
-                alt='second user online'
-                className='w-5 h-5 rounded-full'
-              />
-              <img
-                src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80'
-                alt='third user online'
-                className='w-5 h-5 rounded-full'
-              />
+
+        {gameListState.map((item, index) => {
+          return (
+            <div
+              key={index}
+              onClick={e => dropDownText(index)}
+              href={`${item.id}`}
+              className={`${
+                index === openedFaqId ? 'active' : ''
+              } bonus-area mb-2 rounded-lg flex items-center cursor-pointer`}
+            >
+              <img src={item.Img} alt='card' />
+              <h1 className='flex-1 text-center'>{item.Text}</h1>
             </div>
+          )
+        })}
+
+        <div className='breaker flex-1'></div>
+
+        {supportList.map((item, index) => {
+          return (
+            <div
+              key={index}
+              onClick={e => supportAction(index)}
+              className={`${
+                index === supportActiveState ? 'active' : ''
+              } bonus-area mb-2 rounded-lg flex items-center cursor-pointer`}
+            >
+              <img src={item.Img} alt='card' />
+              <h1 className='flex-1 text-center'>{item.Text}</h1>
+            </div>
+          )
+        })}
+      </aside>
+      <div className='flex-1'>
+        <Header />
+        <div className='body-area flex items-start px-6 pt-6'>
+          <div className='body-left-area  mr-6' ref={ref}>
+            <div className='welcome-area py-8 p-6 rounded-xl relative'>
+              <h1 className='mb-4'>Lucky Spin Tournament</h1>
+              <p className='my-3'>
+                Win in selected Smartoft games. Save points and share{' '}
+              </p>
+              <p className='text-start text-2xl py-4 w-full  rounded-xl rolling my-3 font-extrabold'>
+                ${' '}
+                {rollingNumber
+                  .toString()
+                  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}{' '}
+              </p>
+              <p>1.09-26.09</p>
+              <div className='buttons-wrapper mt-6'>
+                <button className='w-36 h-11 mr-3'>Get Started</button>
+              </div>
+
+              <img src={rewardBanner} className='rewardBanner' alt='' />
+            </div>
+
+            <div className='tags-wrapper mt-10 mb-12  grid grid-cols-7 gap-3'>
+              {gameListState.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={e => dropDownText(index)}
+                    // href={`${item.id}`}
+                    className={`${
+                      index === openedFaqId ? 'active' : ''
+                    } tag flex justify-center  items-center cursor-pointer`}
+                  >
+                    <img src={item.Img} alt='game show' />
+                    <p>{item.Text}</p>
+                  </div>
+                )
+              })}
+            </div>
+
+            {homeState && (
+              <>
+                {/* RecentWin*/}
+                <Hits />
+
+                {/* NewArrival */}
+                <NewArrival />
+                {/* Sports */}
+                <Sports />
+
+                {/* LiveCasino */}
+                <LiveCasino />
+
+                {/* Slots */}
+                <Slots />
+
+                {/* Roulette */}
+                <Roulette />
+
+                {/* Fishing */}
+                <Fishing />
+
+                {/* GameShow */}
+                <GameShow />
+
+                {/* Top Rates Game */}
+                <TopRatesGame />
+
+                {/* LatestBets */}
+                <LatestBets />
+
+                {/* Recommended Game */}
+                <RecommendedGames />
+              </>
+            )}
+
+            {sportState && (
+              <>
+                {/* Sports */}
+                <Sports />
+              </>
+            )}
+
+            {liveState && (
+              <>
+                {/* LiveCasino */}
+                <LiveCasino />
+              </>
+            )}
+            {slotState && (
+              <>
+                {/* Slots */}
+                <Slots />
+              </>
+            )}
+            {rouletteState && (
+              <>
+                {/* Roulette */}
+                <Roulette />
+              </>
+            )}
+            {fishingState && (
+              <>
+                {/* Fishing */}
+                <Fishing />
+              </>
+            )}
+
+            {showState && (
+              <>
+                {/* GameShow */}
+                <GameShow />
+              </>
+            )}
           </div>
+          <div className='body-right-area rounded-2xl p-4'>
+            <div className='top-area pb-4 flex items-center relative'>
+              <img src={chat} alt='' />
+              <h1 className='flex-1 ml-1'>General Chat</h1>
+              <p className='mr-1 absolute'>489</p>
+              <div className='flex items-center'>
+                <img
+                  src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80'
+                  alt='first user online'
+                  className='w-5 h-5 rounded-full'
+                />
+                <img
+                  src='https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80'
+                  alt='second user online'
+                  className='w-5 h-5 rounded-full'
+                />
+                <img
+                  src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80'
+                  alt='third user online'
+                  className='w-5 h-5 rounded-full'
+                />
+              </div>
+            </div>
 
-          <div className='scroll-area-start pt-4 pb-10' ref={refRight}>
-            <img src={dailyBonus} alt='Daily Bonus' className='w-full mb-4' />
+            <div className='scroll-area-start pt-4 pb-10' ref={refRight}>
+              <img src={dailyBonus} alt='Daily Bonus' className='w-full mb-4' />
 
-            {commentsData.map((EachData, index) => (
-              <CommentCard
-                key={index}
-                name={EachData.name}
-                date={EachData.date}
-                img={EachData.img}
-                comment={EachData.comment}
-              />
-            ))}
+              {commentsData.map((EachData, index) => (
+                <CommentCard
+                  key={index}
+                  name={EachData.name}
+                  date={EachData.date}
+                  img={EachData.img}
+                  comment={EachData.comment}
+                />
+              ))}
 
-            <div className='comment-area flex items-center px-4 h-14 rounded-bl-2xl rounded-br-2xl'>
-              <input
-                type='text'
-                placeholder='Send a message...'
-                className='flex-1 w-full'
-              />
-              <div className='flex'>
-                <img src={emoji} alt='emoji' className='mr-3' />
-                <img src={send} alt='send' />
+              <div className='comment-area flex items-center px-4 h-14 rounded-bl-2xl rounded-br-2xl'>
+                <input
+                  type='text'
+                  placeholder='Send a message...'
+                  className='flex-1 w-full'
+                />
+                <div className='flex'>
+                  <img src={emoji} alt='emoji' className='mr-3' />
+                  <img src={send} alt='send' />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import "../assets/css/home.css";
-import { Aside } from "../components/Aside";
-import { RightAreaHome } from "../components/RightAreaHome";
-import { Footer } from "../components/Footer";
+import React from 'react'
+import '../assets/css/home.css'
+import { Aside } from '../components/Aside'
+import { RightAreaHome } from '../components/RightAreaHome'
+import { Footer } from '../components/Footer'
 
 export const Home = () => {
   return (
     <div>
-      <div className="flex items-start home">
+      <div className='flex items-start home'>
         {/* left area */}
-        <Aside />
+        {/* <Aside /> */}
         {/* right area */}
-        <RightAreaHome/>
+        <RightAreaHome />
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
